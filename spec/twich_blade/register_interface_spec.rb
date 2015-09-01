fce552f2a15c65e5639639226ad588b26ed40f60
praveen shukla <praveen@praveens-MacBook-Pro.local>
2015-09-01 13:54:47 +0530

    added spec for username and password.

--- spec/twich_blade/register_interface_spec.rb
+++ spec/twich_blade/register_interface_spec.rb
@@ -6,12 +6,12 @@ module TwichBlade
       it "should able to validate the username" do
         dbname = "test_twichblade"
         interface = RegisterInterface.new(dbname)
-        allow(Kernel).to receive(:gets).and_return("foo")
+        allow(Kernel).to receive(:gets).and_return("foo", "asdasasdsasds")
         interface.display_user_input
         expect(interface.validate?).to eq(true)
       end
 
-      it "should able to validate the username" do
+      it "should able to validate the null username" do
         dbname = "test_twichblade"
         interface = RegisterInterface.new(dbname)
         allow(Kernel).to receive(:gets).and_return("")
@@ -19,7 +19,7 @@ module TwichBlade
         expect(interface.validate?).to eq(false)
       end
 
-      it "should able to validate the username" do
+      it "should able to validate the null password" do
         dbname = "test_twichblade"
         interface = RegisterInterface.new(dbname)
         allow(Kernel).to receive(:gets).and_return("foo", "")
@@ -27,6 +27,21 @@ module TwichBlade
         expect(interface.validate?).to eq(false)
       end
 
+      it "should able to validate the password" do
+        dbname = "test_twichblade"
+        interface = RegisterInterface.new(dbname)
+        allow(Kernel).to receive(:gets).and_return("foo", "barbar1234")
+        interface.display_user_input
+        expect(interface.validate?).to eq(true)
+      end
+
+      it "should able to validate the username and password length validaiton" do
+        dbname = "test_twichblade"
+        interface = RegisterInterface.new(dbname)
+        allow(Kernel).to receive(:gets).and_return("foo", "bar")
+        interface.display_user_input
+        expect(interface.validate?).to eq(false)
+      end
     end
   end
 end
