9d6df56013849849b50f6faa15a299a5614d7dbe
praveen shukla <praveen@praveens-MacBook-Pro.local>
2015-09-01 13:56:33 +0530

    added spec for username and password length validation.

--- spec/twich_blade/register_interface_spec.rb
+++ spec/twich_blade/register_interface_spec.rb
@@ -42,6 +42,14 @@ module TwichBlade
         interface.display_user_input
         expect(interface.validate?).to eq(false)
       end
+
+      it "should able to validate the username and password length validaiton" do
+        dbname = "test_twichblade"
+        interface = RegisterInterface.new(dbname)
+        allow(Kernel).to receive(:gets).and_return("foo", "foobar12345")
+        interface.display_user_input
+        expect(interface.validate?).to eq(true)
+      end
     end
   end
 end
