a2de6a302a1469418a59bdfb1f45e9a98b4d9a56
praveen shukla <praveen@praveens-MacBook-Pro.local>
2015-09-27 16:41:24 +0530

    spec for minutes age meassage

--- lib/twich_blade/time.rb
+++ lib/twich_blade/time.rb
@@ -5,9 +5,12 @@ module TwichBlade
     end
 
     def show
-      diff_in_sec = (Object::Time.new - @given_time).to_i.abs
-      if diff_in_sec <= 59
+      time_difference_in_sec = (Object::Time.new - @given_time).to_i.abs
+      if time_difference_in_sec <= 59
         just_tweeted_meassage
+      elsif time_difference_in_sec > 60 && time_difference_in_sec <= 3600
+        minutes = time_difference_in_sec/60
+        minutes_message(minutes)
       else
         @given_time.to_time.strftime("%d %b %Y")
       end
@@ -15,7 +18,11 @@ module TwichBlade
 
     private
     def just_tweeted_meassage
-      puts "Just Tweeted"
+      puts 'Just Tweeted'
+    end
+
+    def minutes_message(minutes)
+      puts "#{minutes} minutes ago"
     end
   end
 end
--- spec/twich_blade/time_spec.rb
+++ spec/twich_blade/time_spec.rb
@@ -8,6 +8,12 @@ module TwichBlade
         time_object = Time.new(time)
         expect { time_object.show }.to output(/Just Tweeted/).to_stdout
       end
+
+      it 'should be able to give minutues message' do
+        time  = Object::Time.new + 135
+        time_object = Time.new(time)
+        expect { time_object.show }.to output(/2 minutes ago/).to_stdout
+      end
     end
   end
 end
